daf9c7703055bfb325e43530269a460a2baaafc8
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2024-06-04 00:13:52 +0600

    User Registration

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AccountController extends Controller
+{
+    // this method will show user registration page
+    public function registration()
+    {
+        return view('frontend.account.registration');
+    }
+
+    // this method will save a user
+    public function processRegistration(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:5|same:confirm_password',
+            'confirm_password' => 'required',
+        ]);
+
+        if ($validator->passes()) {
+
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            session()->flash('success', 'You have register successfully.');
+            return response()->json([
+                'status' => true,
+                'errors' => [],
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors(),
+            ]);
+        }
+    }
+
+    // this method will show user login page
+    public function login()
+    {
+        return view('frontend.account.login');
+    }
+}
