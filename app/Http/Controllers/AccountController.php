645ec9564ca02b23293b16948987ef700f109c28
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2024-06-06 01:08:52 +0600

    Update Profile Pic

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Image;
 
 class AccountController extends Controller
 {
@@ -75,36 +76,83 @@ class AccountController extends Controller
         return view('frontend.account.profile');
     }
 
-    public function updateProfile(Request $request){
-        $id=Auth::user()->id;
-        $validator=Validator::make($request->all(),[
-            'name'=>'required|string|min:5|max:20',
-            'email'=>'required|email|unique:users,email,'.$id,
+    public function updateProfile(Request $request)
+    {
+        $id = Auth::user()->id;
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|min:5|max:20',
+            'email' => 'required|email|unique:users,email,' . $id,
         ]);
-        if($validator->passes()){
-            $user= User::find($id);
-            $user->name=$request->name;
-            $user->email=$request->email;
-            $user->mobile=$request->mobile;
-            $user->designation=$request->designation;
+        if ($validator->passes()) {
+            $user = User::find($id);
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->mobile = $request->mobile;
+            $user->designation = $request->designation;
             $user->save();
 
-            session()->flash('success','Profile updated successfully.');
+            session()->flash('success', 'Profile updated successfully.');
             return response()->json([
-                'status'=>true,
-                'errors'=>[]
+                'status' => true,
+                'errors' => [],
             ]);
-        }else{
+        } else {
             return response()->json([
-                'status'=>false,
-                'errors'=>$validator->errors()
+                'status' => false,
+                'errors' => $validator->errors(),
             ]);
         }
 
     }
 
-    public function logout(){
+    public function logout()
+    {
         Auth::logout();
         return redirect()->route('account.login');
+    }
+
+    public function updateProfilePic(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image',
+        ]);
+        if ($validator->passes()) {
+            $user = User::findorFail(Auth::user()->id);
+            $this->image_upload($request, $user->id);
+
+            session()->flash('success', 'Profile picture updated successfully.');
+            return response()->json([
+                'status' => true,
+                'errors' => [],
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors(),
+            ]);
+        }
+    }
+
+    public function image_upload($request, $user_id)
+    {
+        $user = User::findorFail($user_id);
+
+        if ($request->hasFile('image')) {
+            //delete old photo
+            if (Auth::user()->image != '') {
+                $photo_location = 'public/profile_pic/';
+                $old_photo_location = $photo_location . $user->image;
+                unlink(base_path($old_photo_location));
+            }
+
+            $photo_loation = 'public/profile_pic/';
+            $uploaded_photo = $request->file('image');
+            $new_photo_name = time() . '.' . $uploaded_photo->getClientOriginalExtension();
+            $new_photo_location = $photo_loation . $new_photo_name;
+            Image::make($uploaded_photo)->resize(600, 600)->save(base_path($new_photo_location));
+            $check = $user->update([
+                'image' => $new_photo_name,
+            ]);
+        }
     }
 }
