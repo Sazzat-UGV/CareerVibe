364b0622e9cdb420bdd484ef81d662f8a5355de5
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2024-06-08 00:52:18 +0600

    Home - Search Jobs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,6 +13,8 @@ class HomeController extends Controller
         $categories=Category::where('status',1)->orderBy('name','ASC')->take(8)->get();
         $featuredJobs=Job::with('jobType:id,name')->where('status',1)->where('isFeatured',1)->orderBy('created_at','DESC')->take(6)->get();
         $latestJobs=Job::with('jobType:id,name')->where('status',1)->orderBy('created_at','DESC')->take(6)->get();
-        return view('frontend.home',compact('categories','featuredJobs','latestJobs'));
+
+        $newCategories=Category::where('status',1)->orderBy('name','ASC')->get();
+        return view('frontend.home',compact('categories','featuredJobs','latestJobs','newCategories'));
     }
 }
